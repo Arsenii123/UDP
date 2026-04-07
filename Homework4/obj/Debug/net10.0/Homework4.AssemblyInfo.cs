0e55bbd796d9afd548ec48e271c6602a1976dfd6
Ars123 <@arsenii310810gmail.com>
2026-04-07 16:27:12 +0300

    Hom4.1

--- Homework4/obj/Debug/net10.0/Homework4.AssemblyInfo.cs
+++ Homework4/obj/Debug/net10.0/Homework4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4540e6283f995376d5f3a56ecc84b4f5fd367a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
